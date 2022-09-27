b5102cabf857650b01a8c8ccdc3e65d8d2f64ace
Rotirgo <kozlov22.00@mail.ru>
2022-09-27 12:01:29 +0400

    filter

--- src/program/utils/Server/Server.java
+++ src/program/utils/Server/Server.java
@@ -7,6 +7,8 @@ import java.awt.image.WritableRaster;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+
 import program.utils.Pixel;
 
 
@@ -27,28 +29,58 @@ public class Server {
 
     private static BufferedImage MedianFilter(BufferedImage srcImg,int [][] mask)
     {
-
-        Pixel [][] pixels = new Pixel [3][3];
+        //Pixel [][] pixels = new Pixel [3][3];
+        int size = 9;
+        Pixel [] pixels = new Pixel [size];
         BufferedImage trgImg = clone(srcImg);
         int height = trgImg.getHeight();
         int width = trgImg.getWidth();
         for(int i=0;i<height;++i) {
             for (int j = 0; j < width; ++j) {
 
-                pixels[0][0] =new Pixel(j-1,i-1,new Color(trgImg.getRGB(j-1,i-1)));
-                pixels[0][1] =new Pixel(j,i-1,new Color(trgImg.getRGB(j,i-1)));
-                pixels[0][2] =new Pixel(j+1,i-1,new Color(trgImg.getRGB(j+1,i-1)));
-                pixels[1][0] =new Pixel(j-1,i,new Color(trgImg.getRGB(j-1,i)));
-                pixels[1][1] =new Pixel(j,i,new Color(trgImg.getRGB(j,i)));
-                pixels[1][2] =new Pixel(j+1,i,new Color(trgImg.getRGB(j+1,i)));
-                pixels[2][0] =new Pixel(j-1,i+1,new Color(trgImg.getRGB(j-1,i+1)));
-                pixels[2][1] =new Pixel(j,i+1,new Color(trgImg.getRGB(j,i+1)));
-                pixels[2][2] =new Pixel(j+1,i+1,new Color(trgImg.getRGB(j+1,i+1)));
+//                pixels[0][0] =new Pixel(j-1,i-1,new Color(trgImg.getRGB(j-1,i-1)));
+//                pixels[0][1] =new Pixel(j,i-1,new Color(trgImg.getRGB(j,i-1)));
+//                pixels[0][2] =new Pixel(j+1,i-1,new Color(trgImg.getRGB(j+1,i-1)));
+//                pixels[1][0] =new Pixel(j-1,i,new Color(trgImg.getRGB(j-1,i)));
+//                pixels[1][1] =new Pixel(j,i,new Color(trgImg.getRGB(j,i)));
+//                pixels[1][2] =new Pixel(j+1,i,new Color(trgImg.getRGB(j+1,i)));
+//                pixels[2][0] =new Pixel(j-1,i+1,new Color(trgImg.getRGB(j-1,i+1)));
+//                pixels[2][1] =new Pixel(j,i+1,new Color(trgImg.getRGB(j,i+1)));
+//                pixels[2][2] =new Pixel(j+1,i+1,new Color(trgImg.getRGB(j+1,i+1)));
 
+                pixels[0] =new Pixel(j-1,i-1,new Color(trgImg.getRGB(j-1,i-1)));
+                pixels[1] =new Pixel(j,i-1,new Color(trgImg.getRGB(j,i-1)));
+                pixels[2] =new Pixel(j+1,i-1,new Color(trgImg.getRGB(j+1,i-1)));
+                pixels[3] =new Pixel(j-1,i,new Color(trgImg.getRGB(j-1,i)));
+                pixels[4] =new Pixel(j,i,new Color(trgImg.getRGB(j,i)));
+                pixels[5] =new Pixel(j+1,i,new Color(trgImg.getRGB(j+1,i)));
+                pixels[6] =new Pixel(j-1,i+1,new Color(trgImg.getRGB(j-1,i+1)));
+                pixels[7] =new Pixel(j,i+1,new Color(trgImg.getRGB(j,i+1)));
+                pixels[8] =new Pixel(j+1,i+1,new Color(trgImg.getRGB(j+1,i+1)));
 
+                int[] redArray = new int[size];
+                int[] greenArray = new int[size];
+                int[] blueArray = new int[size];
 
+                for(int k = 0; k < size; ++k){
+                    redArray[k] = pixels[k].c.getRed();
+                    greenArray[k] = pixels[k].c.getGreen();
+                    blueArray[k] = pixels[k].c.getBlue();
+                }
+                Arrays.sort(redArray);
+                Arrays.sort(greenArray);
+                Arrays.sort(blueArray);
+
+                int medianRed = redArray[size/2];
+                int medianGreen = greenArray[size/2];
+                int medianBlue = blueArray[size/2];
+
+                Color newColor = new Color(medianRed, medianGreen, medianBlue);
+                trgImg.setRGB(j, i, newColor.getRGB());
             }
         }
+
+
         return trgImg;
     }
 
@@ -80,6 +112,7 @@ public class Server {
                             if(word.equals("image"))
                             {
                                 String new_path = path + String.valueOf(counter)+".jpg";
+                                String new_path2 = path + "noise"+String.valueOf(counter)+".jpg";
                                 out = new FileOutputStream(new_path);
                                 System.out.println("Путь до сохраненного изображения :"+"\n"+new_path);
                                 while ((length = in.read(inputByte, 0, inputByte.length)) > 0)
